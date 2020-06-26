f4a0a7c2ca8261a425a98058556d6d697a9ddab0
Marlon Lira <marlonlira3@gmail.com>
2020-06-26 11:35:58 -0300

    fix(pageError): ajustes na pagina de error

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -13,7 +13,7 @@ export const ROUTES: RouteInfo[] = [
   { path: '/employee', title: 'Employee', icon: 'engineering', visible: true },
   { path: '/auth', title: 'auth', icon: 'engineering', visible: false },
   { path: '/user-profile', title: 'User Profile', icon: 'person', visible: true },
-  { path: '/maps', title: 'Maps', icon: 'location_on', visible: true },
+  { path: '/maps', title: 'Maps', icon: 'location_on', visible: true }
 ];
 
 @Component({
