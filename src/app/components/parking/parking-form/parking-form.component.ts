c5b2031454b63d76f0a675a76b3bf14c76df11a3
Marlon Lira <marlonlira3@gmail.com>
2020-07-13 15:40:14 -0300

    fix

--- src/app/components/parking/parking-form/parking-form.component.ts
+++ src/app/components/parking/parking-form/parking-form.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 import { AuthService } from 'app/services/auth.service';
 import { ParkingService } from 'app/services/parking.service';
 import { ToastrService } from 'ngx-toastr';
-import { FormGroup, FormControl } from '@angular/forms';
 import Parking from 'app/models/parking.model';
 import { ParkingComponent } from '../parking.component';
-import { BaseComponent } from 'app/base.component';
 
 @Component({
   selector: 'app-parking-form',
@@ -13,6 +11,8 @@ import { BaseComponent } from 'app/base.component';
   styleUrls: ['./parking-form.component.css']
 })
 export class ParkingFormComponent extends ParkingComponent {
+
+  private _parkingAssign: Parking;
 
   constructor(
     public toastr: ToastrService,
@@ -26,13 +26,17 @@ export class ParkingFormComponent extends ParkingComponent {
 
   }
 
-  onSubmit() {
-    const _value = new Parking(this.form.value);
-    _value.companyId = 1;
-    _value.imgUrl = 'www.google.com.br';
-    const __value = { 'parking': _value };
+  objectBuild() {
+    const obj: Parking = Object.assign({}, this._parkingAssign, this.form.value);
+    const result = { 'parking': {} };
+    obj.imgUrl = 'www.google.com.br';
+    obj.companyId = this.auth.company.id;
+    result.parking = obj;
+    return result;
+  }
 
-    this.parkingService.Save(__value)
+  onSubmit() {
+    this.parkingService.Save(this.objectBuild())
       .then(result => {
         this.toastr.info(result, '');
         this.onResetForm();
