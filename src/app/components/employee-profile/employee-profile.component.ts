9aa348798cfb43e74c71669c687bcbf6093085b9
Emerson Souza <40323952+EmesonSSouza@users.noreply.github.com>
2020-09-03 21:54:16 -0300

    fix(employee-profile): Criação da recuperação de senha do employe

--- src/app/components/employee-profile/employee-profile.component.ts
+++ src/app/components/employee-profile/employee-profile.component.ts
@@ -6,6 +6,7 @@ import { AuthService } from 'app/services/auth.service';
 import { Router } from '@angular/router';
 import Employee from 'app/models/employee.model';
 import { EmployeeService } from 'app/services/employee.service';
+import { Utils } from 'app/commons/core/utils';
 
 @Component({
   selector: 'app-employee-profile',
@@ -43,6 +44,7 @@ export class EmployeeProfileComponent extends BaseComponent {
       registryCode: new FormControl(''),
       phone: new FormControl(''),
       email: new FormControl(''),
+      password: new FormControl(''),
       about: new FormControl(''),
       company: new FormControl({ value: '', disabled: true })
     });
@@ -50,6 +52,9 @@ export class EmployeeProfileComponent extends BaseComponent {
 
   objectBuild() {
     const obj: Employee = Object.assign({}, this.employeeAssign, this.form.value);
+    if(!Utils.isValid(obj.password)){
+      delete obj.password;  
+    }
     return obj;
   }
 
