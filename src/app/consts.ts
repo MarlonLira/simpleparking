e1a25a83e222373c7f660b773d7563c86b7802aa
Marlon Lira <marlonlira3@gmail.com>
2020-09-23 00:48:21 -0300

    fix

--- src/app/consts.ts
+++ src/app/consts.ts
@@ -1,9 +1,9 @@
-// export default {
-//   API_URL: 'https://simplebackend.herokuapp.com',
-//   OAPI_URL: 'https://simplebackend.herokuapp.com/auth',
-// }
-
 export default {
-  API_URL: 'http://localhost:4001',
-  OAPI_URL: 'http://localhost:4001/auth',
+  API_URL: 'https://simplebackend.herokuapp.com',
+  OAPI_URL: 'https://simplebackend.herokuapp.com/auth',
 }
+
+// export default {
+//   API_URL: 'http://localhost:4001',
+//   OAPI_URL: 'http://localhost:4001/auth',
+// }
