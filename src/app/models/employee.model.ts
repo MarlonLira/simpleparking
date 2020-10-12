5253b3eaa32853bed8d5acaca858a4a76d43ab6c
Marlon Lira <marlonlira3@gmail.com>
2020-10-12 16:27:21 -0300

    Fix(employeeProfile): ajustes no employee profile

--- src/app/models/employee.model.ts
+++ src/app/models/employee.model.ts
@@ -21,21 +21,23 @@ export default class Employee {
   public parking: Parking;
 
   constructor(json?: any) {
-    this.id = Utils.returnIfValid(json.id);
-    this.name = Utils.returnIfValid(json.name);
-    this.status = Utils.returnIfValid(json.status);
-    this.registryCode = Utils.returnIfValid(json.registryCode);
-    this.password = Utils.returnIfValid(json.password);
-    this.phone = Utils.returnIfValid(json.phone);
-    this.email = Utils.returnIfValid(json.email);
-    this.about = Utils.returnIfValid(json.about);
-    this.company = Utils.returnIfValid(json.company);
-    this.image = Utils.returnIfValid(json.image);
-    this.parkingId = Utils.returnIfValid(json.parkingId);
-    this.parking = Utils.returnIfValid(json.parking);
-    this.companyId = Utils.returnIfValid(json.companyId);
-    this.ruleId = Utils.returnIfValid(json.ruleId);
-    this.rule = Utils.returnIfValid(json.rule);
+    if (json) {
+      this.id = Utils.returnIfValid(json.id);
+      this.name = Utils.returnIfValid(json.name);
+      this.status = Utils.returnIfValid(json.status);
+      this.registryCode = Utils.returnIfValid(json.registryCode);
+      this.password = Utils.returnIfValid(json.password);
+      this.phone = Utils.returnIfValid(json.phone);
+      this.email = Utils.returnIfValid(json.email);
+      this.about = Utils.returnIfValid(json.about);
+      this.company = Utils.returnIfValid(json.company);
+      this.image = Utils.returnIfValid(json.image);
+      this.parkingId = Utils.returnIfValid(json.parkingId);
+      this.parking = Utils.returnIfValid(json.parking);
+      this.companyId = Utils.returnIfValid(json.companyId);
+      this.ruleId = Utils.returnIfValid(json.ruleId);
+      this.rule = Utils.returnIfValid(json.rule);
+    }
   }
 
 }
