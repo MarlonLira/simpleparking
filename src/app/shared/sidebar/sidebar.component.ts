c1231493ca7b2e3da4f2889eda599e79b16a4f1e
Marlon Lira <marlonlira3@gmail.com>
2020-11-19 11:04:44 -0300

    fix(parkingSelect)

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,6 +3,12 @@ import { BaseComponent } from 'app/base.component';
 import { Router } from '@angular/router';
 import { AuthService } from 'app/services/auth.service';
 import { ToastrService } from 'ngx-toastr';
+import { Authentication } from 'app/commons/enums/authentication';
+import { Utils } from 'app/commons/core/utils';
+import { Crypto } from 'app/commons/core/crypto';
+import Parking from 'app/models/parking.model';
+import { ParkingService } from 'app/services/parking.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 declare const $: any;
 declare interface RouteInfo {
@@ -36,11 +42,13 @@ export const ROUTES: RouteInfo[] = [
 export class SidebarComponent extends BaseComponent {
 
   menuItems: any[];
+  public parkings: Parking[];
 
   constructor(
     public toastr: ToastrService,
     public authService: AuthService,
-    public router: Router) {
+    public router: Router,
+    public parkingService: ParkingService) {
     super(toastr, router, authService);
   }
 
@@ -48,9 +56,57 @@ export class SidebarComponent extends BaseComponent {
   protected onDestroy(): void { }
 
   protected onInit(): void {
+    this.onStartLoading();
+    this.formBuild();
     this.menuItems = ROUTES.filter(menuItem => menuItem);
+    this.onLoadParkings();
   }
-  
+
+  private onLoadParkings() {
+    this.parkingService.toList()
+      .then((result: Parking[]) => {
+        this.parkings = result;
+        this.parkings = this.parkings.reverse()
+        this.parkings.push({ 'id': 0, 'status': 'AT', 'name': 'N/A' } as Parking);
+        this.parkings = this.parkings.reverse();
+        this.onChangeAuth();
+        this.onStopLoading();
+      });
+  }
+
+  formBuild(): void {
+    this.form = new FormGroup({
+      parkingId: new FormControl(0)
+    });
+  }
+
+  public onChange = () => this.onChangeAuth(Number(this.form.value.parkingId));
+
+  private onChangeAuth(parkingId?: number) {
+    this.onStartLoading();
+    const _auth = this.getAuth();
+    let _parkingId = Utils.isValid(parkingId) ? parkingId : (_auth.employee.parkingId ? _auth.employee.parkingId : 0);
+    const _authentication = <Authentication>this.auth.authenticationLevel;
+
+    this.form.controls['parkingId'].setValue(_parkingId);
+
+    if (_authentication != Authentication.Developer && _authentication != Authentication.Administrator) {
+      this.form.disable();
+    } else {
+      this.form.enable();
+    }
+
+    if (this.parkings.length > 0) {
+      _auth.parking = this.parkings.find(x => x.id === _parkingId);
+      _auth.employee.parkingId = _parkingId;
+      this.setAuth(Crypto.encrypt(JSON.stringify(_auth)));
+    }
+
+    if (Utils.isValid(parkingId)) {
+      this.reloadPage();
+    }
+  }
+
   isMobileMenu() {
     if ($(window).width() > 991) {
       return false;
