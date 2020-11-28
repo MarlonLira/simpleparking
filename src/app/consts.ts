a26250183b92bc4ab6823caa822d64f01ade5743
Marlon Lira <marlonlira3@gmail.com>
2020-11-28 12:59:58 -0300

    prod(this)

--- src/app/consts.ts
+++ src/app/consts.ts
@@ -1,13 +1,13 @@
-//  export default {
-//    API_URL: 'https://simplebackend.herokuapp.com',
-//    OAPI_URL: 'https://simplebackend.herokuapp.com/auth',
-//    VIACEP_API_URL: 'https://viacep.com.br/ws',
-//    OSV_API_URL: 'https://nominatim.openstreetmap.org/search.php'
-//  }
-
 export default {
-  API_URL: 'http://localhost:4001',
-  OAPI_URL: 'http://localhost:4001/auth',
+  API_URL: 'https://simplebackend.herokuapp.com',
+  OAPI_URL: 'https://simplebackend.herokuapp.com/auth',
   VIACEP_API_URL: 'https://viacep.com.br/ws',
   OSV_API_URL: 'https://nominatim.openstreetmap.org/search.php'
 }
+
+// export default {
+//   API_URL: 'http://localhost:4001',
+//   OAPI_URL: 'http://localhost:4001/auth',
+//   VIACEP_API_URL: 'https://viacep.com.br/ws',
+//   OSV_API_URL: 'https://nominatim.openstreetmap.org/search.php'
+// }
